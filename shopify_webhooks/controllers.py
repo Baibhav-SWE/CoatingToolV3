ba937191d03ec871566aeb098ac368a97234f0d5
Bansaj Pradhan <bansajpradhan@gmail.com>
2025-01-10 17:46:11 +0545

    add services function for webhook

--- shopify_webhooks/controllers.py
+++ shopify_webhooks/controllers.py
@@ -1,4 +1,7 @@
 from flask import request, jsonify
+from app.services.subscription import create_subscription, check_valid_product
+from app.utils.database import get_users_collection
+from .services import get_required_data_from_webhook_order_fulfillment
 
 
 def handle_product_create():
@@ -14,6 +17,21 @@ def handle_product_delete():
 
 
 def handle_order_fulfillment():
-    order_data = request.json
-    print("Order Fulfilled:", order_data)
+    print("Order Data:", request.json)
+    order_data = get_required_data_from_webhook_order_fulfillment(request.json)
+    if order_data is None:
+        return jsonify({"message": "Order data is missing"}), 400
+    is_valid_product = check_valid_product(str(order_data["product_id"]))
+    print("is_valid_product:", is_valid_product)
+    if not is_valid_product:
+        return jsonify({"message": "Successful"}), 200
+
+    user_collection = get_users_collection()
+    user = user_collection.find_one({"email": order_data["email"]})
+    if user is None:
+        return jsonify({"message": "User not found"}), 404
+    created, message = create_subscription(user["_id"], order_data)
+    print("Subscription Data:", message)
+    if not created:
+        return jsonify({"message": message}), 400
     return jsonify({"message": "Order Fulfilled"}), 200
