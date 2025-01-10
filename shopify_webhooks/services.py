cfa1a773778d5e241e992f2a5f63f84e4d5db310
Bansaj Pradhan <bansajpradhan@gmail.com>
2025-01-10 17:46:11 +0545

    add services function for webhook

---
+++ shopify_webhooks/services.py
@@ -0,0 +1,21 @@
+def get_required_data_from_webhook_order_fulfillment(order_fulfillment):
+    try:
+        return {
+            "order_id": order_fulfillment["id"],
+            "fulfillment_status": order_fulfillment["fulfillment_status"],
+            "email": order_fulfillment["customer"]["email"],
+            "product_id": order_fulfillment["fulfillments"][0]["line_items"][0][
+                "product_id"
+            ],
+            "subscription_duration": order_fulfillment["fulfillments"][0]["line_items"][
+                0
+            ]["variant_title"],
+            "total_price": order_fulfillment["fulfillments"][0]["line_items"][0][
+                "price"
+            ],
+            "product_title": order_fulfillment["fulfillments"][0]["line_items"][0][
+                "title"
+            ],
+        }
+    except KeyError:
+        return None
